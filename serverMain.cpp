7a13120469b6b10585867e1200271018e195f0ca
dvdsvds <kundr5@naver.com>
2025-02-09 22:01:06 +0900

    add handleClient function

--- serverMain.cpp
+++ serverMain.cpp
@@ -1,4 +1,5 @@
 #include <cerrno>
+#include <cstdio>
 #include <cstring>
 #include <iostream>
 #include <string>
@@ -6,8 +7,12 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <thread>
 
 using namespace std;
+
+#define PORT 12345
+#define MAX_CLIENTS 10
 
 void logError(const string& errMsg) {
     cout << "[ERROR] " << errMsg << endl;
@@ -17,15 +22,35 @@ void logInfo(const string& infoMsg) {
     cout << "[INFO] " << infoMsg << endl;
 }
 
-int main() {
+void handleClient(int clientSocket) {
     const int BUFFER_SIZE = 1024;
+    vector<char> buffer(BUFFER_SIZE);
+    int receivedBytes;
+
+    while(true) {
+        receivedBytes = recv(clientSocket, buffer.data(), BUFFER_SIZE, 0);
+        if (receivedBytes == -1) {
+            logError("데이터 수신 실패");
+            logError(strerror(errno));
+            break;
+        } else if (receivedBytes == 0) {
+            logInfo("클라이언트가 연결을 종료했습니다.");
+            break;
+        }
+
+        string receivedData(buffer.begin(), buffer.begin() + receivedBytes);
+        cout << "클라이언트 : " + receivedData << endl;;
+
+
+    }
+    close(clientSocket);
+
+}
+int main() {
     int serverSocket, clientSocket;
     struct sockaddr_in serverAddr;
     socklen_t addrLen = sizeof(serverAddr);
     int backlog = 5;
-
-    vector<char> buffer(BUFFER_SIZE);
-    string message = "Hello";
 
     // 서버 소켓 생성
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -38,7 +63,7 @@ int main() {
     // 서버 주소 설정
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY; // 모든 IP 주소를 수락
-    serverAddr.sin_port = htons(12345);
+    serverAddr.sin_port = htons(PORT);
 
     // 바인딩
     if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
@@ -78,36 +103,17 @@ int main() {
 
     logInfo("클라이언트 연결 완료");
 
-    // 데이터 수신
-    int receivedBytes = recv(clientSocket, buffer.data(), BUFFER_SIZE, 0);
-    if (receivedBytes == -1) {
-        logError("데이터 수신 실패");
-        logError(strerror(errno));
-        close(clientSocket);
-        close(serverSocket);
-        return -1;
-    } else if (receivedBytes == 0) {
-        logInfo("클라이언트가 연결을 종료했습니다.");
-        close(clientSocket);
-        close(serverSocket);
-        return 0;
+    thread clientThread(handleClient, clientSocket);
+    clientThread.detach();
+
+    while(true) {
+        string inputMsg;
+        cout << "> ";
+        getline(cin, inputMsg);
+
+        send(clientSocket, inputMsg.c_str(), inputMsg.size(), 0);
     }
-
-    string receivedData(buffer.begin(), buffer.begin() + receivedBytes);
-    logInfo("받은 메시지 : " + receivedData);
-
-    if(send(clientSocket, message.c_str(), message.length(), 0) == -1) {
-        logError("메시지 전송 실패"); 
-        logError(strerror(errno));
-        logError(strerror(errno));
-        close(clientSocket);
-        return -1; 
-    }
-
-    logInfo("메시지 전송 완료");
-
     // 소켓 종료
-    close(clientSocket);
     close(serverSocket);
 
     return 0;
